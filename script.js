9bfbc6bdd4743cbd95fadf07a12fe84a3f3500f2
fc-pescara <dylanceulemans@gmail.com>
2024-12-30 23:06:21 +0100

    Exercice Imc

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-// je dois prendre les deux donner des input et faire le calcul montant diviser par le nb d'année et 2% en plus du montant
-
- const montant = document.querySelector('.montant')
- const resultat = document.querySelector('.taux')
- const annee = document.querySelector('.annee')
- const button = document.querySelector('.button')
-
- button.addEventListener('click', () => {
-    
-   const valeurAnnee = Number(annee.value); // Convertit la valeur en nombre
-    const result = valeurAnnee ; // ANNEE
-    const montant1 = Number(montant.value) 
-    const Pret = montant1 * 0.02 
-    const CalculOne = result + Pret
-
-    console.log(CalculOne); // Affiche le résultat du taux d'interet
-
-
-const LastCalcul = CalculOne + montant1 // MONTANT INITIAL + TAUX D'INTERET
-
-
-resultat.value = `Résultat : ${LastCalcul.toFixed(2)}`; // Arrondi à deux décimales
+const poids = document.querySelector('.poids');
+const taille = document.querySelector('.taille');
+const button = document.querySelector('.button');
+const resultat = document.querySelector('.resultat');
+const imc = document.querySelector('.imc');
 
 
 
+button.addEventListener('click', () => {
+   
+    const poidsNumber = Number(poids.value); // Convertit la valeur en nombre
+    const tailleNumber = Number(taille.value) / 100; // Convertir en mètres; // Convertit la valeur en nombre
+    const CalculImc = poidsNumber / (tailleNumber * tailleNumber) ; // CALCUL IMC
+
+
+    /// 
+    if (poidsNumber > 0 && tailleNumber > 0) {
+
+
+        imc.textContent  = `Votre IMC est : ${CalculImc.toFixed(2)}`; // Affiche l'IMC avec 2 décimales//
+         }
+
+
+         if  (CalculImc > 12 && CalculImc < 15) {
+            resultat.textContent = "Votre IMC est bon.";
+            resultat.style.color = "green"}
+
+            else if  (CalculImc > 19.51 && CalculImc < 22 ) {
+                resultat.textContent = "Votre IMC est moyen.";
+                resultat.style.color = "orange"} 
+
+               else if  (CalculImc > 22.01 && CalculImc < 25  ) {
+                    resultat.textContent = "Votre IMC est en surpoids.";
+                    resultat.style.color = "blue";}
+
+                 else if     (CalculImc > 25.01 && CalculImc < 30 ) {
+                        resultat.textContent = "Votre IMC est obese.";
+                        resultat.style.color = "red";}
+
+                       else if   (CalculImc > 30.01 ) {
+                            resultat.textContent = "Votre IMC est hyper obese.";
+                            resultat.style.color = "black";}
 
 
 
-})
+         })
+
+
+
+
+
+            
